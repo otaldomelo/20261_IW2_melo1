77453f7dad2e8e9d62b8888ffe196ae3789c27be
Melo <98564448+otaldomelo@users.noreply.github.com>
2026-05-13 10:11:40 -0300

    Add files via upload

--- consulta2.php
+++ consulta2.php
@@ -4,23 +4,62 @@ include "conecta.php";
 
 function inserir($conn, $tamanho, $cor){
 
+    $resultado = "";
+
     if ($conn->query("INSERT INTO tb_camiseta VALUES (NULL, '".$tamanho."', '".$cor."')")){
 
-        echo "
-            <div class='alert alert-success mt-3'>
-                Registro efetuado com sucesso!
+        $resultado .= "
+            <div class='alert alert-success'>
+                Pedido realizado com sucesso!
             </div>
         ";
+
+        $stmt = $conn->query("SELECT tamanho, cor FROM tb_camiseta");
+
+        $resultado .= "
+
+            <table class='table table-striped table-bordered mt-3'>
+
+                <thead class='table-dark'>
+
+                    <tr>
+                        <th>Tamanho</th>
+                        <th>Cor</th>
+                    </tr>
+
+                </thead>
+
+                <tbody>
+        ";
+
+        while ($row = $stmt->fetchObject()) {
+
+            $resultado .= "
+                <tr>
+                    <td>$row->tamanho</td>
+                    <td>$row->cor</td>
+                </tr>
+            ";
+
+        }
+
+        $resultado .= "
+                </tbody>
+            </table>
+        ";
+
+        echo $resultado;
 
     } else {
 
         echo "
-            <div class='alert alert-danger mt-3'>
+            <div class='alert alert-danger'>
                 Registro não efetuado.
             </div>
         ";
 
     }
+
 }
 
 ?>
