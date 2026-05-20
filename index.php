22f7914a5f9fe585d5d8d592da8e404a4d942b55
Melo <98564448+otaldomelo@users.noreply.github.com>
2026-05-20 11:31:37 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -2,15 +2,14 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
+ <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
-        crossorigin="anonymous"></script>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
     <title>Loja de Camisetas</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css"
+        rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script>
 
     $(document).ready(function () {
@@ -57,7 +56,6 @@
 
             }).done(function(resposta){
                 console.log(resposta);
-                // Remove a linha da tabela sem recarregar a página
                 $botao.closest('tr').fadeOut(300, function() {
                     $(this).remove();
                 });
@@ -79,11 +77,8 @@
 <body>
 
     <div class="container form">
-
         <h1>PEDIDO DE CAMISETAS</h1>
-
         <p>Preencha o formulário abaixo para efetuar seu pedido!</p>
-
     </div>
 
     <form id="pedidoForm" action="insere.php" method="post">
@@ -131,6 +126,129 @@
     </form>
 
     <div id="resultado"></div>
+
+    <!-- css novo(tenho ansiedade)-->
+
+    
+<!--
+       <div id="registro">
+        <button type="button"
+            id="bregistro"
+            class="btn botao-registro"
+            data-bs-toggle="modal"
+            data-bs-target="#modalCadastro">
+
+            Adicionar Pedido
+        </button>
+    </div>
+
+-->
+
+    <!-- coisinho do Cadastro -->
+<!--
+    <div class="modal" id="modalCadastro">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header modal-superior">
+                    <h4 class="modal-title">
+                        Cadastro de Camiseta
+                    </h4>
+
+                    <button type="button"
+                        class="btn-close"
+                        data-bs-dismiss="modal"></button>
+
+                </div>
+                <div class="modal-body modal-interior">
+
+  
+                        <h2>Cor:</h2>
+
+                        <input type="text"
+                            class="cor entrada-texto"
+                            placeholder="Ex: Preto, Branco...">
+
+                        <br><br>
+
+                        <h2>Tamanho:</h2>
+
+                        <select class="tamanho entrada-texto">
+                            <option value="PP">PP</option>
+                            <option value="P">P</option>
+                            <option value="M">M</option>
+                            <option value="G">G</option>
+                            <option value="GG">GG</option>
+                        </select>
+                </div>
+                <div class="modal-footer modal-inferior">
+                    <button class="botao-registro"
+                        id="confirma">
+                        Enviar
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+-->
+
+ <!--coisinho de editar-->
+
+ <!--
+    <div class="modal" id="modalEdita">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header modal-superior">
+                    <h4 class="modal-title">
+                        Editar Pedido
+                    </h4>
+                    <button type="button"
+                        class="btn-close"
+                        data-bs-dismiss="modal"></button>
+
+                </div>
+
+                <div class="modal-body modal-interior">
+                        <h2>Cor:</h2>
+
+                        <input type="text"
+                            class="editaCor entrada-texto">
+
+                        <br><br>
+
+                        <h2>Tamanho:</h2>
+                        <select class="editaTamanho entrada-texto">
+                            <option value="PP">PP</option>
+                            <option value="P">P</option>
+                            <option value="M">M</option>
+                            <option value="G">G</option>
+                            <option value="GG">GG</option>
+                        </select>
+                </div>
+
+                <div class="modal-footer modal-inferior">
+                    <button type="button"
+                        class="btn save-button"
+                        data-bs-dismiss="modal">
+                        Salvar
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="lista-cards" class="d-flex flex-wrap gap-3">
+
+-->
+
+<!--a função não tá funcionando, resolve depois-->
+        <?php
+
+        include "consulta.php";
+
+        ?>
+        
+    </div>
+
 
 </body>
 
